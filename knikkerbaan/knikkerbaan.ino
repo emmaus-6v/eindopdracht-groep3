0b2a0fc8e4802192475af6be2cbd9792ef0daaab
R-venna <111752@emmauscollege.nl>
2020-11-25 10:57:53 +0000

    tijdelijk onnodige code verwijderd

--- knikkerbaan/knikkerbaan.ino
+++ knikkerbaan/knikkerbaan.ino
@@ -1,21 +1,9 @@
 #include <Servo.h>
 
-const int ledPin =  LED_BUILTIN;
-
 int i = 0;
 
-// globale variabelen
-int ledState = LOW;               // status van de LED
-
 Servo servo_10;
-
 Servo servo_11;
-
-
-unsigned long millisVorigeVerandering = 0; // bewaard tijd van laatste LED-verandering
-
-// andere constanten
-const long interval = 1000;           // interval in milliseconden
 
 void setup() {
   // stel ledpin in als output:
@@ -26,62 +14,27 @@ void setup() {
 
   pinMode(4, INPUT);
   servo_11.attach(11);
-<<<<<<< HEAD
-
-=======
->>>>>>> 233906f5f37d28b0780d7ef392387b274b4e1f8c
 }
 
 void loop() {
-  // tijd van nu
-  unsigned long millisNu = millis();
+  
+ if (digitalRead(7) == HIGH) {
+  servo_10.write(90);
+ }
+ if (digitalRead(7) == LOW) {
+  servo_10.write(0);
+ }
 
-  // is de verstreken tijd sinds de laatste verandering langer dan de interval?
-  if (millisNu - millisVorigeVerandering >= interval) {    
-    // bewaar tijdstip van de verandering die we hierna gaan doen
-    millisVorigeVerandering = millisNu;
-  }
-    // zet de status van de LED om
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-        if (digitalRead(7) == HIGH) {
-            servo_10.write(90);
-          }
-          if (digitalRead(7) == LOW) {
-            servo_10.write(0);
-          }
-
-          if (digitalRead(4) == HIGH) {
-            servo_11.write(90);
-          }
-          if (digitalRead(4) == LOW) {
-            servo_11.write(0);
-          }
-          delay(10); //Voor simulatie verbeteren 
-    // geef de pin de waarde van de status
-    digitalWrite(ledPin, ledState);
-
-   if (digitalRead(7) == HIGH) {
-    servo_10.write(90);
-   }
-   if (digitalRead(7) == LOW) {
-    servo_10.write(0);
-   }
-
-   if (digitalRead(4) == HIGH) {
-    servo_11.write(90);
-   }
-   if (digitalRead(4) == LOW) {
-    servo_11.write(0);
-   }
-   delay(10); //Voor simulatie verbeteren 
-
+ if (digitalRead(4) == HIGH) {
+  servo_11.write(90);
+ }
+ if (digitalRead(4) == LOW) {
+  servo_11.write(0);
+ }
+ delay(10); //Voor simulatie verbeteren 
   
 }
 
 
 //hoii
+//hola
