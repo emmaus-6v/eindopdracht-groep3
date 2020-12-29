c3f8ac897327968ffb54e9efa052e2e9de6123dd
Lennard-Bus <111605@emmauscollege.nl>
2020-12-29 14:00:20 +0000

    milis() toegevoegd + random servo + servo knopje

--- knikkerbaan/knikkerbaan.ino
+++ knikkerbaan/knikkerbaan.ino
@@ -1,11 +1,16 @@
 #include <Servo.h>
 
+const unsigned long moveSensorInterval =  random(3000, 6000 + 1);
+unsigned long previousTime = 0;
+
 int i = 0;
 
 Servo servo_10;
 Servo servo_11;
 
 void setup() {
+
+  Serial.begin(9600);
 
   pinMode(7, INPUT);
   servo_10.attach(10);
@@ -17,31 +22,45 @@ void setup() {
 void loop() {
 <<<<<<< HEAD
   // tijd van nu
-  unsigned long millisNu = millis();
-
-  // is de verstreken tijd sinds de laatste verandering langer dan de interval?
-  if (millisNu - millisVorigeVerandering >= interval) {    
-    // bewaar tijdstip van de verandering die we hierna gaan doen
-    millisVorigeVerandering = millisNu;
-
-    // zet de status van de LED om
+  
+  /* Updates frequently */
+unsigned long currentTime = millis();
+  
+  /* This is the event*/
+  if( currentTime - previousTime >= moveSensorInterval){
+    
+    if(moveSensorInterval <= 4500){
+      servo_11.write(90);
+    }
+    else {
+      servo_11.write(0);
+    }
+    
     if (ledState == LOW) {
       ledState = HIGH;
     } else {
       ledState = LOW;
     }
+    digitalWrite(ledPin, ledState);
 
-     digitalWrite(ledPin, ledState);
+    previousTime = currentTime;
+
+  }
+  
+   
+
+     
 
         if (digitalRead(7) == HIGH) {
     servo_10.write(90);
   } else {
     servo_10.write(0);
   }
-  if (digitalRead(4) == HIGH) {
+  /*if (digitalRead(4) == LOW) {
     servo_11.write(90);
+
   } else {
-    servo_11.write(0);
+    servo_11.write(0); */
   }
   delay(10); //Voor simulatie verbeteren 
     // geef de pin de waarde van de status
