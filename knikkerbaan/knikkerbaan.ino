799cad18b90877ba9da6d446f8a0fb6eba4a733b
LarsNoordman01 <noordmanlars@gmail.com>
2021-03-01 18:59:23 +0000

    thomas' ding er in geplakt

--- knikkerbaan/knikkerbaan.ino
+++ knikkerbaan/knikkerbaan.ino
@@ -83,7 +83,41 @@ unsigned long currentTime = millis();
  }
  Serial.println(marbleCount);  
 
-    
+    int red, green, blue;
+    GroveColorSensor colorSensor;
+    colorSensor.ledStatus = 1;            // Wanneer de color sensor LED aangaat gaat de ledStatus = 1; en wanneer uit naar ledStatus = 0;.
+    while(1){
+        colorSensor.readRGB(&red, &green, &blue);    //leest RGB waarde naar variabelen.
+        delay(300);
+        Serial.print("The RGB value are: RGB( ");
+        Serial.print(red,DEC);
+        Serial.print(", ");
+        Serial.print(green,DEC);
+        Serial.print(", ");
+        Serial.print(blue,DEC);
+        Serial.println(" )");
+        colorSensor.clearInterrupt();
+    }
+    var colorsensor.statuts = no.color; // omdat hij nog niks scanned word ->geen kleur
+  if(colorSensor.ledStatus = 1){
+    if(colorSensor.ledStatus = 1 & colorsensor.readRGB = red){
+      colorsensor.status = red; // als hij rood balletje ziet dan geeft hij rode status omdat door te geven aan scherm om daar de kleur te tonen
+    }
+    if(colorSensor.ledStatus = 1 & colorsensor.readRGB = green){
+      colorsensor.status = green; // zelfde geld hier maar dan groen
+    }
+    if(colorSensor.ledStatus = 1 & colorsensor.readRGB = bleu){
+      colorsensor.status = bleu; // hier ook maar dan blauw
+    }
+     if(colorSensor.ledStatus = 1 & colorsensor.readRGB = bleu & red & bleu){
+      colorsensor.status = white; // omdat wit alle kleuren weg kaatst moeten alle kleuren worden opgevangen en daarom wit
+    }
+  }
+  else{
+    colorsensor.status = no.color; //als die geen groen of of rood of blauw balletje ziet dan is dr geen color
+  }
+  /*dit moest verbonden worden aan schermpje maar dat wilt niet werken dus gaat niet en thomas heeft geen arduino stuf om te testen of het wel werkt
+  maar dat gaat ook moeilijk zonder scherm*/
 }
 
 
